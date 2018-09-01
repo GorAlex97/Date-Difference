63b0a8482b2785143baa52421ec2789794523d1e
goralex97 <5267720@gmail.com>
2018-09-01 23:42:27 +0000

    Added web-version

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 	require_once 'vendor/autoload.php';
 	use GorAlex97\Dates\VagrantPhp;	
 
-	$opts_d = getopt('d:');
-	$opts_n = getopt('n:');
+	$opts_d = @$_GET['d']?@$_GET['d']:'1997-06-06';
+	$opts_n = @$_GET['n']?@$_GET['n']:'1997-06-06';
 	
-	if(isset($opts_d['d'])){
-		$birthDate = $opts_d['d'] ?? '1997-06-06';
+	if(isset($opts_d)){
+		$birthDate = $opts_d ?? '1997-06-06';
 		$days = new VagrantPhp($birthDate);
-	}elseif(isset($opts_n['n'])){
+	}elseif(isset($opts_n)){
 		$names = parse_ini_file(__DIR__.'/src/names.ini',true);
-		$birthDate = $names['dates'][$opts_n['n']];
+		$birthDate = $names['dates'][$opts_n];
 		$days = new VagrantPhp($birthDate);
 	}else{
 		$birthDate = '1997-06-06';
